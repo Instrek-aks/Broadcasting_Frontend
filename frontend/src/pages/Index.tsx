8306f08dbd5281b8f482ae75b2bb05a6f37eed27
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2026-01-14 19:59:05 +0530

    remove proice section

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServiceSection";
-import PricingSection from "../components/PriceSection";
+// import PricingSection from "../components/PriceSection";
 import Footer from "../components/Footer";
 
 const Index = () => {
